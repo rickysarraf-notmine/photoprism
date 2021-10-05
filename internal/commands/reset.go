f5ef36b8e2fc0383ea829ea36a5d2ca146124747
Krassimir Valev <krassimir_valev@yahoo.com>
2021-10-05 16:45:45 +0200

    MP: Extend the reset command to remove transcoded videos and motion photo videos

--- internal/commands/reset.go
+++ internal/commands/reset.go
@@ -11,6 +11,7 @@ import (
 	"github.com/manifoldco/promptui"
 	"github.com/photoprism/photoprism/internal/config"
 	"github.com/photoprism/photoprism/internal/entity"
+	"github.com/photoprism/photoprism/pkg/fs"
 	"github.com/urfave/cli"
 )
 
@@ -131,6 +132,50 @@ func resetAction(ctx *cli.Context) error {
 		log.Infof("keeping backup files")
 	}
 
+	removeSidecarVideosPrompt := promptui.Prompt{
+		Label:     "Permanently delete all transcoded videos, incl. extracted motion photo videos?",
+		IsConfirm: true,
+	}
+
+	if _, err := removeSidecarVideosPrompt.Run(); err == nil {
+		start := time.Now()
+
+		mp4Matches, err := filepath.Glob(regexp.QuoteMeta(conf.SidecarPath()) + "/**/*.mp4")
+
+		if err != nil {
+			return err
+		}
+
+		// the avc files and burried deep in the sidecar folder, so we need to search it recursively
+		avcMatches, err := recursiveGlob(conf.SidecarPath(), fs.AvcExt)
+
+		if err != nil {
+			return err
+		}
+
+		matches := append(mp4Matches, avcMatches...)
+
+		if len(matches) > 0 {
+			log.Infof("%d transcoded videos will be removed", len(matches))
+
+			for _, name := range matches {
+				if err := os.Remove(name); err != nil {
+					fmt.Print("E")
+				} else {
+					fmt.Print(".")
+				}
+			}
+
+			fmt.Println("")
+
+			log.Infof("removed files in %s", time.Since(start))
+		} else {
+			log.Infof("no transcoded videos found for removal")
+		}
+	} else {
+		log.Infof("keeping transcoded videos")
+	}
+
 	removeAlbumYamlPrompt := promptui.Prompt{
 		Label:     "Permanently delete all *.yml album backups?",
 		IsConfirm: true,
@@ -169,4 +214,18 @@ func resetAction(ctx *cli.Context) error {
 	conf.Shutdown()
 
 	return nil
+}
+
+// recursiveGlob recursiverly searches the given root folder for files matching the given extension.
+func recursiveGlob(root string, extension string) ([]string, error) {
+	matches := []string{}
+
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if filepath.Ext(path) == extension {
+			matches = append(matches, path)
+		}
+		return nil
+	})
+
+	return matches, err
 }
