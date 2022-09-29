35e723a40d9e4483fd5b004daf8f31d4deb373ea
Krassimir Valev <krassimir_valev@yahoo.com>
2022-09-29 19:09:08 +0200

    Sharing: Use album thumbnail as share preview image
    If the shared album has a thumbnail its only natural that it is used as a share
    preview image. Additional checks are done to ensure that the thumbnail image is
    not private, hidden or archived.

--- internal/api/share_preview.go
+++ internal/api/share_preview.go
@@ -16,6 +16,7 @@ import (
 	"github.com/photoprism/photoprism/internal/entity"
 	"github.com/photoprism/photoprism/internal/form"
 	"github.com/photoprism/photoprism/internal/photoprism"
+	"github.com/photoprism/photoprism/internal/query"
 	"github.com/photoprism/photoprism/internal/search"
 	"github.com/photoprism/photoprism/internal/service"
 	"github.com/photoprism/photoprism/internal/thumb"
@@ -71,6 +72,42 @@ func SharePreview(router *gin.RouterGroup) {
 			return
 		}
 
+		if album.HasThumb() {
+			f, err := query.FileByHash(album.Thumb)
+
+			if err != nil {
+				log.Errorf("share: %s (retrieve thumbnail file for album %s)", err, &album)
+				c.Redirect(http.StatusTemporaryRedirect, conf.SitePreview())
+				return
+			}
+
+			p := f.RelatedPhoto()
+
+			if !p.PhotoPrivate && p.DeletedAt == nil {
+				size, _ := thumb.Sizes[thumb.Fit720]
+
+				fileName := photoprism.FileName(f.FileRoot, f.FileName)
+
+				if !fs.FileExists(fileName) {
+					log.Errorf("share: file %s is missing (thumbnail preview)", clean.Log(f.FileName))
+					c.Redirect(http.StatusTemporaryRedirect, conf.SitePreview())
+					return
+				}
+
+				thumbnail, err := thumb.FromFile(fileName, f.FileHash, conf.ThumbCachePath(), size.Width, size.Height, f.FileOrientation, size.Options...)
+
+				if err != nil {
+					log.Error(err)
+					c.Redirect(http.StatusTemporaryRedirect, conf.SitePreview())
+					return
+				}
+
+				c.File(thumbnail)
+
+				return
+			}
+		}
+
 		var f form.SearchPhotos
 
 		// Covers may only contain public content in shared albums.
@@ -111,7 +148,7 @@ func SharePreview(router *gin.RouterGroup) {
 			fileName := photoprism.FileName(f.FileRoot, f.FileName)
 
 			if !fs.FileExists(fileName) {
-				log.Errorf("share: file %s is missing (preview)", clean.Log(f.FileName))
+				log.Errorf("share: file %s is missing (single file preview)", clean.Log(f.FileName))
 				c.Redirect(http.StatusTemporaryRedirect, conf.SitePreview())
 				return
 			}
@@ -141,7 +178,7 @@ func SharePreview(router *gin.RouterGroup) {
 			fileName := photoprism.FileName(f.FileRoot, f.FileName)
 
 			if !fs.FileExists(fileName) {
-				log.Errorf("share: file %s is missing (preview)", clean.Log(f.FileName))
+				log.Errorf("share: file %s is missing (collage preview)", clean.Log(f.FileName))
 				c.Redirect(http.StatusTemporaryRedirect, conf.SitePreview())
 				return
 			}
