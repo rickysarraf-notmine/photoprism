1317a53b86eccb2ac109601474eac41f1e56d30a
Krassimir Valev <krassimir_valev@yahoo.com>
2023-05-11 13:47:30 +0300

    ACL: Family role with (mostly) read-only permissions
    Add a new role to be used by family members. It provides a read only to the
    instance - photos, moments, places, calendar, people.
    The role also grants one "edit" permission - to like photos, the reason being to
    simplify selecting the best vacation shots.

--- internal/acl/acl_events.go
+++ internal/acl/acl_events.go
@@ -3,14 +3,17 @@ package acl
 // Events specifies granted permissions by event channel and Role.
 var Events = ACL{
 	ResourceDefault: Roles{
-		RoleAdmin: GrantFullAccess,
+		RoleAdmin:  GrantFullAccess,
+		RoleFamily: GrantSubscribeOwn,
 	},
 	ChannelUser: Roles{
 		RoleAdmin:   GrantFullAccess,
+		RoleFamily:  GrantSubscribeOwn,
 		RoleVisitor: GrantSubscribeOwn,
 	},
 	ChannelSession: Roles{
 		RoleAdmin:   GrantFullAccess,
+		RoleFamily:  GrantSubscribeOwn,
 		RoleVisitor: GrantSubscribeOwn,
 	},
 }
