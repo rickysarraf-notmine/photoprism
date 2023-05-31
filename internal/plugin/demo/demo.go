991e71bff9d24bb34da497ea2d05cd53615587f3
Krassimir Valev <krassimir_valev@yahoo.com>
2023-06-01 00:54:05 +0300

    Plugin: Improve demo plugin code style

--- internal/plugin/demo/demo.go
+++ internal/plugin/demo/demo.go
@@ -11,11 +11,11 @@ func (p DemoPlugin) Name() string {
 	return "demo"
 }
 
-func (DemoPlugin) Configure(plugin.PluginConfig) error {
+func (*DemoPlugin) Configure(plugin.PluginConfig) error {
 	return nil
 }
 
-func (DemoPlugin) OnIndex(file *entity.File, photo *entity.Photo) error {
+func (*DemoPlugin) OnIndex(file *entity.File, photo *entity.Photo) error {
 	photo.Details.Notes = "hello from demo plugin"
 	photo.Details.NotesSrc = entity.SrcManual
 
